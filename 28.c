61c2cafbe459aeae215c60bd4d99a811f2056c5a
naveenbalaji <30828376+naveenbalaj@users.noreply.github.com>
2018-01-23 16:24:32 +0530

    Create 28.c

---
+++ 28.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int a[50],i,n;
+	printf("enter the array size");
+	scanf("%d",&n);
+	printf("\n enter the array element");
+	for(i=0;i<n;i++)
+	{
+	scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+	printf("\n %d %d",i,a[i]);
+	}
+	return (0);
+}
